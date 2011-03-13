43f0cc5550c676788393bb42159a07f4793c1866
Michael Ries <michael@riesd.com>
2011-03-13 06:24:34 -0700

    initial commit of mongoid powered rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mblog::Application.config.secret_token = 'a5de741af7d4a0aaf5cb2c1395ffe6aa86c3bae21c0b6a54bd5114cb12a6702361b3a094dd776443e62b69adc4d3745455f577d93dad8639ef372d418dce6bdf'
